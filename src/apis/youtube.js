9f855061b2872842fb8532275bf87f2353dfb9ed
nvirkar <ninadvirkar61@gmail.com>
2024-01-19 15:17:18 +0530

    added key

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = ""; // enter your key here
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
